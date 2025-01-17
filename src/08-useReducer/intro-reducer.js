605cffa0690e9f2155da7eb60047f5b2744e8d50
andresunda <and_ua@hotmail.com>
2025-01-17 15:05:51 -0600

    Bassic lesson to understand the functionament of useReducer

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -1,1 +1,36 @@
-console.log("Hola mundo")
+const { type } = require("@testing-library/user-event/dist/type");
+
+const initialState = [{
+    id: 1,
+    todo: 'Recolectar la piedra del Alma',
+    done: false,
+}];
+
+
+const todoReducer = (state = initialState, action = {}) => {
+
+    if (action.type === '[TODO] add todo') {
+        return [...state, action.payload];
+    }
+
+    return state;
+}
+
+
+let todos = todoReducer();
+
+const newTodo = {
+    id: 2,
+    todo: 'Recolectar la piedra del Poder',
+    done: false,
+}
+
+const addTodoAction = {
+    type: '[TODO] add todo',
+    payload: newTodo,
+}
+
+todos = todoReducer(todos, addTodoAction);
+
+
+console.table({ state: todos });
