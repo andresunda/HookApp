1c42c0fe6d65a60df25b3ad268dbd5c7083fa44c
andresunda <and_ua@hotmail.com>
2025-01-22 11:51:27 -0600

    Practice to understand useContext, Seccion 11 Fernando Herrera

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './context/UserContext';
 
 export const LoginPage = () => {
+
+    const { user, setUser } = useContext(UserContext);
+    //console.log(algo);
+
     return (
         <>
             <h1>Login page</h1>
             <hr />
+
+            <pre>
+                {JSON.stringify(user, null, 3)}
+            </pre>
+
+
+            <button
+                className='btn btn-primary'
+                onClick={() => setUser({ id: 123, name: 'Andres', email: 'undadevcode@gmail.com' })}
+            >
+                Establecer usuario
+            </button>
         </>
     )
 }
